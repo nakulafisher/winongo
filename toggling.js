c0e635d3519167e6e6289e0c4cfba0289d31ec21
nakulafisher <nakulafisher@gmail.com>
2015-04-21 04:45:48 +0700

    toggle

---
+++ toggling.js
@@ -0,0 +1,551 @@
+
+	$("#layerbangunan").prop('checked', true);
+	
+	function checkbangunan () {
+	if ($("#layerbangunan").prop('checked')==true) {
+        bangunan.setVisibility(true);
+    } else {
+        bangunan.setVisibility(false);
+    }
+	};
+	$("#layerbangunan").change(function event(){checkbangunan()});
+	
+	
+	$("#layerbair").prop('checked', true);
+	
+	function checkbair () {
+	if ($("#layerbair").prop('checked')==true) {
+        bair.setVisibility(true);
+    } else {
+        bair.setVisibility(false);
+    }
+	};
+	$("#layerbair").change(function event(){checkbair()});
+	
+	
+	$("#layerbanjir").prop('checked', true);
+	
+	function checkbanjir () {
+	if ($("#layerbanjir").prop('checked')==true) {
+        banjir.setVisibility(true);
+    } else {
+        banjir.setVisibility(false);
+    }
+	};
+	$("#layerbanjir").change(function event(){checkbanjir()});
+	
+	
+	$("#layerevakuasi").prop('checked', true);
+	
+	function checkevakuasi () {
+	if ($("#layerevakuasi").prop('checked')==true) {
+        evakuasi.setVisibility(true);
+    } else {
+        evakuasi.setVisibility(false);
+    }
+	};
+	$("#layerevakuasi").change(function event(){checkevakuasi()});
+	
+		
+	$("#layerhydrant").prop('checked', true);
+	
+	function checkhydrant () {
+	if ($("#layerhydrant").prop('checked')==true) {
+        hydrant.setVisibility(true);
+    } else {
+        hydrant.setVisibility(false);
+    }
+	};
+	$("#layerhydrant").change(function event(){checkhydrant()});
+	
+	
+	$("#layeripal").prop('checked', true);
+	
+	function checkipal () {
+	if ($("#layeripal").prop('checked')==true) {
+        ipal.setVisibility(true);
+    } else {
+        ipal.setVisibility(false);
+    }
+	};
+	$("#layeripal").change(function event(){checkipal()});
+	
+	
+	$("#layeririgasi").prop('checked', true);
+	
+	function checkirigasi () {
+	if ($("#layeririgasi").prop('checked')==true) {
+        irigasi.setVisibility(true);
+    } else {
+        irigasi.setVisibility(false);
+    }
+	};
+	$("#layeririgasi").change(function event(){checkirigasi()});
+	
+	
+	$("#layerjalan").prop('checked', true);
+	
+	function checkjalan () {
+	if ($("#layerjalan").prop('checked')==true) {
+        jalan.setVisibility(true);
+    } else {
+        jalan.setVisibility(false);
+    }
+	};
+	$("#layerjalan").change(function event(){checkjalan()});
+	
+	
+	$("#layerjapol").prop('checked', true);
+	
+	function checkjapol () {
+	if ($("#layerjapol").prop('checked')==true) {
+        japol.setVisibility(true);
+    } else {
+        japol.setVisibility(false);
+    }
+	};
+	$("#layerjapol").change(function event(){checkjapol()});
+	
+	
+	$("#layerkebakaran").prop('checked', true);
+	
+	function checkkebakaran () {
+	if ($("#layerkebakaran").prop('checked')==true) {
+        kebakaran.setVisibility(true);
+    } else {
+        kebakaran.setVisibility(false);
+    }
+	};
+	$("#layerkebakaran").change(function event(){checkkebakaran()});
+	
+	
+	$("#layerkecamatan").prop('checked', true);
+	
+	function checkkecamatan () {
+	if ($("#layerkecamatan").prop('checked')==true) {
+        kecamatan.setVisibility(true);
+    } else {
+        kecamatan.setVisibility(false);
+    }
+	};
+	$("#layerkecamatan").change(function event(){checkkecamatan()});
+	
+	
+	$("#layerkelurahan").prop('checked', true);
+	
+	function checkkelurahan () {
+	if ($("#layerkelurahan").prop('checked')==true) {
+        kelurahan.setVisibility(true);
+    } else {
+        kelurahan.setVisibility(false);
+    }
+	};
+	$("#layerkelurahan").change(function event(){checkkelurahan()});
+	
+	
+	$("#layerkumpul").prop('checked', true);
+	
+	function checkkumpul () {
+	if ($("#layerkumpul").prop('checked')==true) {
+        kumpul.setVisibility(true);
+    } else {
+        kumpul.setVisibility(false);
+    }
+	};
+	$("#layerkumpul").change(function event(){checkkumpul()});
+	
+	
+	$("#layerkumpulgempa").prop('checked', true);
+	
+	function checkkumpulgempa () {
+	if ($("#layerkumpulgempa").prop('checked')==true) {
+        kumpulgempa.setVisibility(true);
+    } else {
+        kumpulgempa.setVisibility(false);
+    }
+	};
+	$("#layerkumpulgempa").change(function event(){checkkumpulgempa()});
+	
+	
+	$("#layerlimbah").prop('checked', true);
+	
+	function checklimbah () {
+	if ($("#layerlimbah").prop('checked')==true) {
+        limbah.setVisibility(true);
+    } else {
+        limbah.setVisibility(false);
+    }
+	};
+	$("#layerlimbah").change(function event(){checklimbah()});
+	
+	
+	$("#layerlongsor").prop('checked', true);
+	
+	function checklongsor () {
+	if ($("#layerlongsor").prop('checked')==true) {
+        longsor.setVisibility(true);
+    } else {
+        longsor.setVisibility(false);
+    }
+	};
+	$("#layerlongsor").change(function event(){checklongsor()});
+	
+	
+	$("#layermck").prop('checked', true);
+	
+	function checkmck () {
+	if ($("#layermck").prop('checked')==true) {
+        mck.setVisibility(true);
+    } else {
+        mck.setVisibility(false);
+    }
+	};
+	$("#layermck").change(function event(){checkmck()});
+	
+	
+	$("#layernonpam").prop('checked', true);
+	
+	function checknonpam () {
+	if ($("#layernonpam").prop('checked')==true) {
+        nonpam.setVisibility(true);
+    } else {
+        nonpam.setVisibility(false);
+    }
+	};
+	$("#layernonpam").change(function event(){checknonpam()});
+	
+	
+	$("#layerpam").prop('checked', true);
+	
+	function checkpam () {
+	if ($("#layerpam").prop('checked')==true) {
+        pam.setVisibility(true);
+    } else {
+        pam.setVisibility(false);
+    }
+	};
+	$("#layerpam").change(function event(){checkpam()});
+	
+	
+	$("#layerpedes").prop('checked', true);
+	
+	function checkpedes () {
+	if ($("#layerpedes").prop('checked')==true) {
+        pedes.setVisibility(true);
+    } else {
+        pedes.setVisibility(false);
+    }
+	};
+	$("#layerpedes").change(function event(){checkpedes()});
+	
+	
+	$("#layerpersil").prop('checked', true);
+	
+	function checkpersil () {
+	if ($("#layerpersil").prop('checked')==true) {
+        persil.setVisibility(true);
+    } else {
+        persil.setVisibility(false);
+    }
+	};
+	$("#layerpersil").change(function event(){checkpersil()});
+	
+	
+	$("#layerrth").prop('checked', true);
+	
+	function checkrth () {
+	if ($("#layerrth").prop('checked')==true) {
+        rth.setVisibility(true);
+    } else {
+        rth.setVisibility(false);
+    }
+	};
+	$("#layerrth").change(function event(){checkrth()});
+	
+	
+	$("#layerrw").prop('checked', true);
+	
+	function checkrw () {
+	if ($("#layerrw").prop('checked')==true) {
+        rw.setVisibility(true);
+    } else {
+        rw.setVisibility(false);
+    }
+	};
+	$("#layerrw").change(function event(){checkrw()});
+	
+	
+	$("#layersah").prop('checked', true);
+	
+	function checksah () {
+	if ($("#layersah").prop('checked')==true) {
+        sah.setVisibility(true);
+    } else {
+        sah.setVisibility(false);
+    }
+	};
+	$("#layersah").change(function event(){checksah()});
+	
+	
+	$("#layersal").prop('checked', true);
+	
+	function checksal () {
+	if ($("#layersal").prop('checked')==true) {
+        sal.setVisibility(true);
+    } else {
+        sal.setVisibility(false);
+    }
+	};
+	$("#layersal").change(function event(){checksal()});
+	
+	
+	$("#layersampah").prop('checked', true);
+	
+	function checksampah () {
+	if ($("#layersampah").prop('checked')==true) {
+        sampah.setVisibility(true);
+    } else {
+        sampah.setVisibility(false);
+    }
+	};
+	$("#layersampah").change(function event(){checksampah()});
+	
+	
+	$("#layersegmen").prop('checked', true);
+	
+	function checksegmen () {
+	if ($("#layersegmen").prop('checked')==true) {
+        segmen.setVisibility(true);
+    } else {
+        segmen.setVisibility(false);
+    }
+	};
+	$("#layersegmen").change(function event(){checksegmen()});
+	
+	
+	$("#layersultan_ground").prop('checked', true);
+	
+	function checksultan_ground () {
+	if ($("#layersultan_ground").prop('checked')==true) {
+        sultan_ground.setVisibility(true);
+    } else {
+        sultan_ground.setVisibility(false);
+    }
+	};
+	$("#layersultan_ground").change(function event(){checksultan_ground()});
+	
+	
+	$("#layersungai").prop('checked', true);
+	
+	function checksungai () {
+	if ($("#layersungai").prop('checked')==true) {
+        sungai.setVisibility(true);
+    } else {
+        sungai.setVisibility(false);
+    }
+	};
+	$("#layersungai").change(function event(){checksungai()});
+	
+	
+	$("#layertaludperumahan").prop('checked', true);
+	
+	function checktaludperumahan () {
+	if ($("#layertaludperumahan").prop('checked')==true) {
+        taludperumahan.setVisibility(true);
+    } else {
+        taludperumahan.setVisibility(false);
+    }
+	};
+	$("#layertaludperumahan").change(function event(){checktaludperumahan()});
+	
+	
+	$("#layertaludsung").prop('checked', true);
+	
+	function checktaludsung () {
+	if ($("#layertaludsung").prop('checked')==true) {
+        taludsung.setVisibility(true);
+    } else {
+        taludsung.setVisibility(false);
+    }
+	};
+	$("#layertaludsung").change(function event(){checktaludsung()});
+	
+	
+	$("#layertps").prop('checked', true);
+	
+	function checktps () {
+	if ($("#layertps").prop('checked')==true) {
+        tps.setVisibility(true);
+    } else {
+        tps.setVisibility(false);
+    }
+	};
+	$("#layertps").change(function event(){checktps()});
+	
+	
+	$("#layerubiogas").prop('checked', true);
+	
+	function checkubiogas () {
+	if ($("#layerubiogas").prop('checked')==true) {
+        ubiogas.setVisibility(true);
+    } else {
+        ubiogas.setVisibility(false);
+    }
+	};
+	$("#layerubiogas").change(function event(){checkubiogas()});
+	
+	
+	$("#layerufumum").prop('checked', true);
+	
+	function checkufumum () {
+	if ($("#layerufumum").prop('checked')==true) {
+        ufumum.setVisibility(true);
+    } else {
+        ufumum.setVisibility(false);
+    }
+	};
+	$("#layerufumum").change(function event(){checkufumum()});
+	
+	
+	$("#layeruhydrant").prop('checked', true);
+	
+	function checkuhydrant () {
+	if ($("#layeruhydrant").prop('checked')==true) {
+        uhydrant.setVisibility(true);
+    } else {
+        uhydrant.setVisibility(false);
+    }
+	};
+	$("#layeruhydrant").change(function event(){checkuhydrant()});
+	
+	
+	$("#layeruipal").prop('checked', true);
+	
+	function checkuipal () {
+	if ($("#layeruipal").prop('checked')==true) {
+        uipal.setVisibility(true);
+    } else {
+        uipal.setVisibility(false);
+    }
+	};
+	$("#layeruipal").change(function event(){checkuipal()});
+	
+	
+	$("#layerumck").prop('checked', true);
+	
+	function checkumck () {
+	if ($("#layerumck").prop('checked')==true) {
+        umck.setVisibility(true);
+    } else {
+        umck.setVisibility(false);
+    }
+	};
+	$("#layerumck").change(function event(){checkumck()});
+	
+	
+	$("#layerumonsungai").prop('checked', true);
+	
+	function checkumonsungai () {
+	if ($("#layerumonsungai").prop('checked')==true) {
+        umonsungai.setVisibility(true);
+    } else {
+        umonsungai.setVisibility(false);
+    }
+	};
+	$("#layerumonsungai").change(function event(){checkumonsungai()});
+	
+	
+	$("#layerunonpam").prop('checked', true);
+	
+	function checkunonpam () {
+	if ($("#layerunonpam").prop('checked')==true) {
+        unonpam.setVisibility(true);
+    } else {
+        unonpam.setVisibility(false);
+    }
+	};
+	$("#layerunonpam").change(function event(){checkunonpam()});
+
+	
+	$("#layerupedestrian").prop('checked', true);
+	
+	function checkupedestrian () {
+	if ($("#layerupedestrian").prop('checked')==true) {
+        upedestrian.setVisibility(true);
+    } else {
+        upedestrian.setVisibility(false);
+    }
+	};
+	$("#layerupedestrian").change(function event(){checkupedestrian()});
+	
+	
+	$("#layerupoint").prop('checked', true);
+	
+	function checkupoint () {
+	if ($("#layerupoint").prop('checked')==true) {
+        upoint.setVisibility(true);
+    } else {
+        upoint.setVisibility(false);
+    }
+	};
+	$("#layerupoint").change(function event(){checkupoint()});
+	
+	
+	$("#layerurth").prop('checked', true);
+	
+	function checkurth () {
+	if ($("#layerurth").prop('checked')==true) {
+        urth.setVisibility(true);
+    } else {
+        urth.setVisibility(false);
+    }
+	};
+	$("#layerurth").change(function event(){checkurth()});
+	
+	
+	$("#layerusah").prop('checked', true);
+	
+	function checkusah () {
+	if ($("#layerusah").prop('checked')==true) {
+        usah.setVisibility(true);
+    } else {
+        usah.setVisibility(false);
+    }
+	};
+	$("#layerusah").change(function event(){checkusah()});
+	
+	
+	$("#layerusal").prop('checked', true);
+	
+	function checkusal () {
+	if ($("#layerusal").prop('checked')==true) {
+        usal.setVisibility(true);
+    } else {
+        usal.setVisibility(false);
+    }
+	};
+	$("#layerusal").change(function event(){checkusal()});
+	
+	
+	$("#layerutalmukim").prop('checked', true);
+	
+	function checkutalmukim () {
+	if ($("#layerutalmukim").prop('checked')==true) {
+        utalmukim.setVisibility(true);
+    } else {
+        utalmukim.setVisibility(false);
+    }
+	};
+	$("#layerutalmukim").change(function event(){checkutalmukim()});
+	
+	
+	$("#layerutalsungai").prop('checked', true);
+	
+	function checkutalsungai () {
+	if ($("#layerutalsungai").prop('checked')==true) {
+        utalsungai.setVisibility(true);
+    } else {
+        utalsungai.setVisibility(false);
+    }
+	};
+	$("#layerutalsungai").change(function event(){checkutalsungai()});
